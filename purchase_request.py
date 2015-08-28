cfa63f7c9d8b65ddf0f160bc070479967c60521e
resteve <resteve@zikzakmedia.com>
2015-08-28 12:11:07 +0200

    Module name header file

--- purchase_request.py
+++ purchase_request.py
@@ -1,4 +1,4 @@
-# This file is part of the stock_supply_supplier_description module for Tryton.
+# This file is part of the purchase_margin module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import PoolMeta
