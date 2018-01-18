523a143401b52c15bd632be533350e540e89f65f
Raimon Esteve <raimon@nan-tic.com>
2018-01-18 10:39:11 +0100

    Set list_price from get_sale_price and not recompute with on_change_product() task-033221 Through third modules set purchase line with new values and on_change_product() reset values calculated with compute_purchase_line()

--- purchase_request.py
+++ purchase_request.py
@@ -1,19 +1,27 @@
 # This file is part of the purchase_margin module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
 
 __all__ = ['CreatePurchase']
-__metaclass__ = PoolMeta
 
 
 class CreatePurchase:
+    __metaclass__ = PoolMeta
     __name__ = 'purchase.request.create_purchase'
 
     @classmethod
     def compute_purchase_line(cls, request, purchase):
-        '''Create purchase line with supplier code and description'''
-        line = super(CreatePurchase, cls).compute_purchase_line(request,
-            purchase)
-        line.on_change_product()
+        Product = Pool().get('product.product')
+
+        with Transaction().set_context(uom=request.uom.id,
+                customer=request.party.id,
+                currency=request.currency.id):
+            prices = Product.get_sale_price(
+                [request.product], request.quantity or 0)
+
+        line = super(CreatePurchase, cls).compute_purchase_line(
+            request, purchase)
+        line.list_price = prices[request.product.id]
         return line
